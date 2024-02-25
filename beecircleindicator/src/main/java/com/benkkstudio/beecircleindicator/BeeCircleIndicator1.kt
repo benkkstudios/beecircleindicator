d692753c23f6cebef045605780ef202c8c7db431
abenkdh <abenkdh@gmail.com>
2024-02-25 10:56:30 +0800

    Initial commit

---
+++ beecircleindicator/src/main/java/com/benkkstudio/beecircleindicator/BeeCircleIndicator1.kt
@@ -0,0 +1,11 @@
+package com.benkkstudio.beecircleindicator
+
+import android.content.Context
+import android.util.AttributeSet
+import com.benkkstudio.beecircleindicator.indicator.CircleIndicator
+
+class BeeCircleIndicator1 : CircleIndicator {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attr: AttributeSet) : super(context, attr)
+    constructor(context: Context, attr: AttributeSet, defStyleAttr: Int) : super(context, attr, defStyleAttr)
+}
---
+++ beecircleindicator/src/main/java/com/benkkstudio/beecircleindicator/BeeCircleIndicator2.kt
@@ -0,0 +1,12 @@
+package com.benkkstudio.beecircleindicator
+
+import android.content.Context
+import android.util.AttributeSet
+import com.benkkstudio.beecircleindicator.indicator.CircleIndicator2
+
+
+class BeeCircleIndicator2 : CircleIndicator2 {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attr: AttributeSet) : super(context, attr)
+    constructor(context: Context, attr: AttributeSet, defStyleAttr: Int) : super(context, attr, defStyleAttr)
+}
